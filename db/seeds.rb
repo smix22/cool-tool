bc45654ce415c719dd90f23b38b4ed1d22975d27
Alejandro Contreras <contrerasx@gmail.com>
2024-01-30 15:11:39 +0100

    seeding complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(email: "user@user.com", first_name: "Dareos", last_name: "Khalili", id: 1, password: "password")
+User.create(email: "owner@owner.com", first_name: "Arbi", last_name: "Milan", id: 2, password: "password")
+
+Tool.create(name: "Hammer", category: "handheld tool", details: "2020 Devault Hammer, with wood handle", location: "Santiago, Chile", user_id: 1)
+Tool.create(name: "Drill", category: "house tool", details: "Red Hilti 2019 power drill", location: "Liverpool, UK", user_id: 1)
+Tool.create(name: "Lawnmower", category: "garden tool", details: "100cc power Lawnmower, 2017, gasoline", location: "Hannover, Germany", user_id: 2)
+Tool.create(name: "Jigsaw", category: "woodwork", details: "1 meter long jigsaw for trees", location: "Berlin, Germany", user_id: 2)
