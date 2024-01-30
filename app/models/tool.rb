e9e7964cca011731ea60d7938e6705c07155ee20
Stefan Mixdorf <smixdorf@gmail.com>
2024-01-30 14:39:46 +0100

    added validations and associations

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,8 @@
 class Tool < ApplicationRecord
   belongs_to :user
+
+  has_many :bookings
+  has_many :users, through: :bookings
+
+  validates :name, :category, :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings
+  has_many :tools, through: :bookings, dependent: :destroy
+
+  validates :first_name, :last_name, :email, presence: true
 end
