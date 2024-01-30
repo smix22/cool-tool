6e963b2144f2cad6ee43b20cbea3f2e38ca26f07
Alejandro Contreras <contrerasx@gmail.com>
2024-01-30 13:00:23 +0100

    booking model and controller ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_114126) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_115612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.text "request"
+    t.boolean "requested"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "tool_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tool_id"], name: "index_bookings_on_tool_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "tools", force: :cascade do |t|
     t.string "name"
@@ -39,5 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_114126) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "tools"
+  add_foreign_key "bookings", "users"
   add_foreign_key "tools", "users"
 end
